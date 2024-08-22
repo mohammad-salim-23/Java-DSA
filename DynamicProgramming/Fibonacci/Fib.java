b0463bf391abbac253d32129c57485158ba68ef3
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-08-22 11:44:42 +0600

    find fibonacci by Tabulatio technique

--- DynamicProgramming/Fibonacci/Fib.java
+++ DynamicProgramming/Fibonacci/Fib.java
@@ -10,9 +10,18 @@ public class Fib{
         f[n] = fib(n-1,f)+fib(n-2,f);
         return f[n];
     }
+    public static int fibTabulation(int n){
+      int dp[] = new int[n+1];
+      dp[1] = 1;
+      for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+      }
+      return dp[n];
+    }
     public static void main(String args[]){
         int n=6;
         int f[] = new int[n+1];
         System.out.println(fib(n,f));
+        System.out.println(fibTabulation(n));
     }
 }
