525b378ee646811faeccea18856badb9169e6eec
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-08-28 23:39:16 +0600

    Weight change

--- AlgorithmCourse/FractionalKnapsack/FractionalKnapsack.java
+++ AlgorithmCourse/FractionalKnapsack/FractionalKnapsack.java
@@ -4,7 +4,7 @@ public class FractionalKnapsack{
     public static void main(String args[]){
         int val[] = {60,100,120};
         int weight[] = {10,20,30};
-        int w = 50;
+        int w = 60;
         double ratio[][] = new double[val.length][2];
         // 0th col=>idx; 1st col=>ratio
         for(int i=0;i<val.length;i++){
