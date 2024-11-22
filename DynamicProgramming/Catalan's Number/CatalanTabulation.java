a3e2c41e81b4b1242e8765758abea922b055c47b
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-11-22 15:20:57 +0600

    Catalan Tabulation

---
+++ DynamicProgramming/Catalan's Number/CatalanTabulation.java
@@ -0,0 +1,17 @@
+public class CatalanTabulation{
+    public static int catalanTab(int n){
+        int dp[] = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+          for(int j=0;j<i;j++){
+            dp[i]+=dp[j]*dp[i-1-j];
+          }  
+        }
+        return dp[n];
+    }
+    public static void main(String args[]){
+        int n = 4;
+        System.out.println(catalanTab(n));
+    }
+}
